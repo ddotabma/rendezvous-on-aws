cb28d5d6a233af145ea870b61b00d92c9bdee7b3
Dick <dick.abma@bigdatarepublic.nl>
2020-03-04 13:40:51 +0100

    add lambda basic execution role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,7 +1,7 @@
 data "aws_iam_policy_document" "lambda_main" {
   version = "2012-10-17"
   statement {
-    sid = ""
+    sid = "lambdamain"
     effect = "Allow"
     actions = [
       "sns:Publish",
@@ -16,7 +16,7 @@ data "aws_iam_policy_document" "lambda_main" {
 data "aws_iam_policy_document" "lambda_sqs" {
   version = "2012-10-17"
   statement {
-    sid = ""
+    sid = "lambdasqs"
     effect = "Allow"
     actions = [
       "sqs:ReceiveMessage",
@@ -28,7 +28,6 @@ data "aws_iam_policy_document" "lambda_sqs" {
     ]
   }
 }
-
 
 data "aws_iam_policy_document" "lambda_main_assume_role" {
   statement {
@@ -44,18 +43,13 @@ data "aws_iam_policy_document" "lambda_main_assume_role" {
 
 
 resource "aws_iam_role" "lambda_main" {
+  name = "lambda_sns_role"
   assume_role_policy = data.aws_iam_policy_document.lambda_main_assume_role.json
 }
-
 
 resource "aws_iam_policy" "lambda_main" {
   name = "lambda_merger"
   policy = data.aws_iam_policy_document.lambda_main.json
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_main" {
-  policy_arn = aws_iam_policy.lambda_main.arn
-  role = aws_iam_role.lambda_main.name
 }
 
 resource "aws_iam_policy" "lambda_sqs" {
@@ -63,9 +57,21 @@ resource "aws_iam_policy" "lambda_sqs" {
   policy = data.aws_iam_policy_document.lambda_sqs.json
 }
 
+// attachments
+
+resource "aws_iam_role_policy_attachment" "lambda_main" {
+  policy_arn = aws_iam_policy.lambda_main.arn
+  role = aws_iam_role.lambda_main.name
+}
+
+
 resource "aws_iam_role_policy_attachment" "lambda_sqs" {
   policy_arn = aws_iam_policy.lambda_sqs.arn
   role = aws_iam_role.lambda_main.name
 }
 
 
+resource "aws_iam_role_policy_attachment" "execution_role" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role = aws_iam_role.lambda_main.id
+}
