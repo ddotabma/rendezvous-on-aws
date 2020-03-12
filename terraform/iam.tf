bf0191632e525b184abf0705556a513f32387f41
Dick <dick.abma@bigdatarepublic.nl>
2020-03-12 08:16:31 +0100

    replace aws_iam_policy by aws_iam_role_policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -48,29 +48,19 @@ resource "aws_iam_role" "lambda_main" {
   assume_role_policy = data.aws_iam_policy_document.lambda_main_assume_role.json
 }
 
-resource "aws_iam_policy" "lambda_main" {
+resource "aws_iam_role_policy" "lambda_main" {
   name = "lambda_merger"
   policy = data.aws_iam_policy_document.lambda_main.json
+  role = aws_iam_role.lambda_main.id
 }
 
-resource "aws_iam_policy" "lambda_sqs" {
+resource "aws_iam_role_policy" "lambda_sqs" {
   name = "lambda_sqs"
   policy = data.aws_iam_policy_document.lambda_sqs.json
+  role = aws_iam_role.lambda_main.id
 }
 
 // attachments
-
-resource "aws_iam_role_policy_attachment" "lambda_main" {
-  policy_arn = aws_iam_policy.lambda_main.arn
-  role = aws_iam_role.lambda_main.name
-}
-
-
-resource "aws_iam_role_policy_attachment" "lambda_sqs" {
-  policy_arn = aws_iam_policy.lambda_sqs.arn
-  role = aws_iam_role.lambda_main.name
-}
-
 
 resource "aws_iam_role_policy_attachment" "execution_role" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
