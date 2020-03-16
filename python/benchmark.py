bba44df48f03ce3391f45fa6784db638958db906
Dick <dick.abma@bigdatarepublic.nl>
2020-03-16 07:54:13 +0100

    add benchmark with httpx

---
+++ python/benchmark.py
@@ -0,0 +1,25 @@
+import httpx
+from typing import List
+import asyncio
+import os
+
+
+async def multiple_get_requests(url_list: List[str]):
+    async with httpx.AsyncClient(timeout=20) as client:
+        return await asyncio.gather(*[get(client, i) for i in url_list])
+
+
+async def get(session: httpx.AsyncClient, url: str) -> List[dict]:
+    response = await session.get(url)  # Make single http request
+    return response.json()  # Obtain response body as dict
+
+
+results = asyncio.run(
+    multiple_get_requests(
+        [os.environ["API"] for _ in range(2)]  # todo make var
+    )
+)
+
+print(results)
+
+print(f"{len(results)} responses")
